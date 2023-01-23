e8483cf18397f35c3deeda929d440225c8481607
tom99763 <69715105+tom99763@users.noreply.github.com>
2023-01-23 16:21:36 +0800

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,23 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+def random():
+  pass
+
+def reference():
+  pass
+
+def feature():
+  pass
+
+class VisualizeCallback(callbacks.Callback):
+    def __init__(self, params, opt, ds_val):
+        super().__init__()
+        self.ds = ds_val
+        self.opt = opt
+        self.params_ = params
+
+    def on_epoch_end(self, epoch, logs=None):
+        random(params, opt, ds_val)
+        reference(params, opt, ds_val)
+        feature(params, opt, ds_val)
